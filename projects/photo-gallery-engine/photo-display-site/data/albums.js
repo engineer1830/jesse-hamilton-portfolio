a3bc403179536508c32ab31f2b73d18c5509e7ae
engineer1830 <keepsmiling.hamilton@gmail.com>
2026-04-13 22:35:56 -0700

    push

---
+++ projects/photo-gallery-engine/photo-display-site/data/albums.js
@@ -0,0 +1,15 @@
+const albums = [
+    {
+        title: "Sample Album 1",
+        slug: "sample-album-1",
+        cover: "albums/sample-album-1/img1.jpg",
+        count: 3
+    },
+    {
+        title: "Sample Album 2",
+        slug: "sample-album-2",
+        cover: "albums/sample-album-2/img1.jpg",
+        count: 3
+    }
+];
+  
