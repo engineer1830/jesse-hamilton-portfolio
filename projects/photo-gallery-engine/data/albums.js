8157956a3b5f633b3afd6dc3436d5d71228c16f3
engineer1830 <keepsmiling.hamilton@gmail.com>
2026-04-14 16:09:33 -0700

    push

--- projects/photo-gallery-engine/data/albums.js
+++ projects/photo-gallery-engine/data/albums.js
@@ -1,11 +1,20 @@
 const albums = [
     {
-        slug: "my-album",
-        title: "My Album",
-        cover: "albums/my-album/cover.jpg",
-        description: "A collection of moments from our trip.",
+        slug: "sample-album-1",
+        title: "Sample Album 1",
+        cover: "albums/sample-album-1/img1.jpg",
+        description: "A collection of sample images.",
         date: "2024",
         location: "Arizona",
-        count: photos["my-album"]?.length || 0
+        count: photos["sample-album-1"]?.length || 0
+    },
+    {
+        slug: "sample-album-2",
+        title: "Sample Album 2",
+        cover: "albums/sample-album-2/img1.jpg",
+        description: "Another set of sample images.",
+        date: "2024",
+        location: "Arizona",
+        count: photos["sample-album-2"]?.length || 0
     }
 ];
--- projects/photo-gallery-engine/data/photos.js
+++ projects/photo-gallery-engine/data/photos.js
@@ -1,13 +1,11 @@
-const photos = {
-    "sample-album-1": [
-        "albums/sample-album-1/img1.jpg",
-        "albums/sample-album-1/img2.jpg",
-        "albums/sample-album-1/img3.jpg"
-    ],
-    "sample-album-2": [
-        "albums/sample-album-2/img1.jpg",
-        "albums/sample-album-2/img2.jpg",
-        "albums/sample-album-2/img3.jpg"
-    ]
-};
-  
+photos["sample-album-1"] = [
+    "albums/sample-album-1/img1.jpg",
+    "albums/sample-album-1/img2.jpg",
+    "albums/sample-album-1/img3.jpg"
+];
+
+photos["sample-album-2"] = [
+    "albums/sample-album-2/img1.jpg",
+    "albums/sample-album-2/img2.jpg",
+    "albums/sample-album-2/img3.jpg"
+];
