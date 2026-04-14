f3dbca7e0ab251cc7d21e615849d967d20d786a7
engineer1830 <keepsmiling.hamilton@gmail.com>
2026-04-14 16:12:20 -0700

    push

--- projects/photo-gallery-engine/data/photos.js
+++ projects/photo-gallery-engine/data/photos.js
@@ -1,9 +1,14 @@
+// Create the global photos object
+const photos = {};
+
+// Sample Album 1
 photos["sample-album-1"] = [
     "albums/sample-album-1/img1.jpg",
     "albums/sample-album-1/img2.jpg",
     "albums/sample-album-1/img3.jpg"
 ];
 
+// Sample Album 2
 photos["sample-album-2"] = [
     "albums/sample-album-2/img1.jpg",
     "albums/sample-album-2/img2.jpg",
